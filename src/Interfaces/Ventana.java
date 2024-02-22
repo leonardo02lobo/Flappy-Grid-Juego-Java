ea453c6260b467c9296d3f527888c9f4f600596b
Leonardo <loboleonardo75@gmail.com>
2024-02-22 15:36:33 -0400

    creacion de la animacion del pajaro

--- src/Interfaces/Ventana.java
+++ src/Interfaces/Ventana.java
@@ -1,10 +1,41 @@
 package Interfaces;
 
-public class Ventana extends Panel{
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public class Ventana extends Panel {
+    int aux = 0;
+
+    public Ventana() {
+        Timer tiempo = new Timer(100, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                init();
+            }
+        });
+        tiempo.start();
+    }
 
     @Override
     public void init() {
-
+        Pajaro();
     }
-    
+
+    private void Pajaro(){
+        
+        Image[] imagenes = {
+            new ImageIcon(getClass().getResource("../source/pajaro_con_fondo-removebg-preview.png")).getImage(),
+            new ImageIcon(getClass().getResource("../source/pngwing.com.png")).getImage()};
+        aux++;
+        if (aux % 2 == 0) {
+            pajaro.setIcon(new ImageIcon(imagenes[0].getScaledInstance(100, 100, Image.SCALE_SMOOTH)));
+        } else {
+            pajaro.setIcon(new ImageIcon(imagenes[1].getScaledInstance(100, 100, Image.SCALE_SMOOTH)));
+        }
+        pajaro.setBounds(40, 20, 100, 100);
+        add(pajaro);
+    }
+    JLabel pajaro = new JLabel();
 }
