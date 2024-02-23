2bce8d19714302a41c07deecc679c3fa01777948
Leonardo <loboleonardo75@gmail.com>
2024-02-22 21:10:46 -0400

    creacion de la gravedad del pajaro

--- src/Interfaces/Pajaro.java
+++ src/Interfaces/Pajaro.java
@@ -1,31 +1,37 @@
 package Interfaces;
 
-import java.awt.Image;
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class Pajaro {
-    
-    public JLabel Pajaro(){
-        
+
+    public JLabel Pajaro() {
+
         Image[] imagenes = {
             new ImageIcon(getClass().getResource("../source/pajaro_con_fondo-removebg-preview.png")).getImage(),
             new ImageIcon(getClass().getResource("../source/pngwing.com.png")).getImage()};
         aux++;
         if (aux % 2 == 0) {
-            pajaro.setIcon(new ImageIcon(imagenes[0].getScaledInstance(100, 100, Image.SCALE_SMOOTH)));
+            pajaro.setIcon(new ImageIcon(imagenes[0].getScaledInstance(ancho, alto, Image.SCALE_SMOOTH)));
         } else {
-            pajaro.setIcon(new ImageIcon(imagenes[1].getScaledInstance(100, 100, Image.SCALE_SMOOTH)));
+            pajaro.setIcon(new ImageIcon(imagenes[1].getScaledInstance(ancho, alto, Image.SCALE_SMOOTH)));
         }
         pajaro.setBounds(x, y, ancho, alto);
         return pajaro;
     }
-    
+
+    public void MovimientosPajaro(KeyEvent e) {
+        if (e.getKeyCode() == 32) {
+            y -= movimiento;
+        }
+    }
+
     private int aux = 0;
-    private final int x = 40;
-    private int y = 20;
+    private final int x = 80;
+    public static int y = 200;
+    public static final int movimiento = 20;
     private final int ancho = 100;
     private final int alto = 100;
     JLabel pajaro = new JLabel();
-    
 }
--- src/Interfaces/Ventana.java
+++ src/Interfaces/Ventana.java
@@ -1,22 +1,29 @@
 package Interfaces;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 import javax.swing.*;
 
 public class Ventana extends Panel {
-    
+
     Pajaro pajaro = new Pajaro();
     Fondo fondo = new Fondo();
 
     public Ventana() {
-        Timer tiempo = new Timer(100, new ActionListener() {
+        Timer tiempo = new Timer(17, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 init();
+                Pajaro.y += Pajaro.movimiento;
             }
         });
         tiempo.start();
+        addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                pajaro.MovimientosPajaro(e);
+            }
+        });
+        setFocusable(true);
     }
 
     @Override
